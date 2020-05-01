8068d375c6e46e99ced202a7210cfcbcda776d81
Adin Castro <acastro@agrobiotek.com>
2020-05-01 15:28:09 -0600

    add calendar test

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,14 +1,38 @@
 import React from "react"
 import { Link } from "gatsby"
 
+import { Row, Col, Card, CardBody } from 'reactstrap';
+import Loadable from 'react-loadable';
+
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+
+const LoadableCallendar = Loadable({
+  loader: () => import('../components/calendar'),
+  loading() {
+    return <div>Loading...</div>
+  },
+})
+
 
 const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <Row className="page-title align-items-center">
+      <Col sm={4} xl={6}>
+        <h4 className="mb-1 mt-0">Calendario</h4>
+      </Col>
+    </Row>
+    <Row>
+      <Col className="col-12">
+        <Card>
+          <CardBody>
+            {/* fullcalendar control */}
+            <LoadableCallendar />
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
