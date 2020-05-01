7758e94ea103f21849de931f41811a2e638b4ea4
Adin Castro <acastro@agrobiotek.com>
2020-05-01 13:48:55 -0600

    first commit

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,14 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.onCreateWebpackConfig = ({ actions, getConfig, stage }) => {
+  const config = getConfig();
+
+  const coreJs2config = config.resolve.alias['core-js'];
+  delete config.resolve.alias['core-js']
+  config.resolve.alias[`core-js/modules`] = `${coreJs2config}/modules`
+  try {
+    config.resolve.alias[`core-js/es`] = path.dirname(require.resolve('core-js/es'));
+  } catch (err) { }
+  actions.replaceWebpackConfig(config);
+};
+
